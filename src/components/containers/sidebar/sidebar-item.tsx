2f41ac4357bc9a84d25487064ab4aff188e4791c
Sijan Mainali <sijanmainali2@gmail.com>
2024-04-11 22:07:39 -0500

    just checkin........

--- src/components/containers/sidebar/sidebar-item.tsx
+++ src/components/containers/sidebar/sidebar-item.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function SidebarItem() {
   return (
-    <div className='min-h-screen flex flex-col justify-between overflow-hidden'>
+    <div className='min-h-screen flex flex-col justify-between'>
       <div className='h-20 flex flex-col justify-between'>
         <div className='flex flex-1 p-4 items-center justify-between'>
           <button>
@@ -21,12 +21,11 @@ function SidebarItem() {
         <div className='bg-gray-300 h-[1px]'></div>
         <div className='flex items-center justify-between p-2 '>
           <div className='flex items-center justify-between space-x-2'>
-            <div className='h-16 w-16 rounded-md bg-red-200 flex items-center justify-center'>
+            <div className='size-12 lg:size-16 rounded-md bg-red-200 flex items-center justify-center'>
               S M
             </div>
             <h1 className='font-bold'>Sijan</h1>
           </div>
-
           <ChevronUpIcon width={24} height={24} />
         </div>
       </div>
