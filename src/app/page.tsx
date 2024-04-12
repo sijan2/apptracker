336a7990bc1257af0596cd0d3fd883c7f3e24985
Sijan Mainali <sijanmainali2@gmail.com>
2024-04-12 01:04:49 -0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,39 @@
 import { Sidebar } from '@/components/containers/sidebar/sidebar'
 import SidebarItem from '@/components/containers/sidebar/sidebar-item'
 import Toggle from '@/components/containers/sidebar/sidebar-toggle'
-import { useSidebar } from '@/lib/hooks/use-sidebar'
+import Card from '@/components/ui/card'
 
 export default function Home() {
   return (
-    <div className='flex relative min-h-full w-full z-0'>
+    <div className='flex relative min-h-full w-full z-0 '>
       <Sidebar className='min-h-screen z-30 hidden  border-r bg-muted duration-300 ease-in-out data-[state=closed]:w-0 data-[state=closed]:hidden lg:flex w-[300px] bg-gray-50'>
         {/* @ts-ignore */}
         <SidebarItem />
       </Sidebar>
-      <div className='relative flex h-full max-w-full flex-1 flex-col overflow-hidden'>
-        <main className='relative min-h-screen w-full flex flex-1 overflow-auto transition-width items-center justify-center'>
-          <Toggle />
-          Hiiiii
-        </main>
-      </div>
+      <main className='relative min-h-screen w-full flex flex-col flex-1 transition-width pb-4 px-4 lg:px-16 '>
+        <Toggle />
+        <div className='flex-col lg:pt-4'>
+          <h1 className='font-bold text-2xl'>Your Submitted Applications</h1>
+          <h1 className='text-bold text-gray-500'>15 TOTAL APPLICATIONS</h1>
+          <div className='bg-gray-300 h-[1px] lg:mt-2'></div>
+        </div>
+        <div className='relative  focus-within:z-10 mt-4'>
+          <input
+            className='block h-11 w-full rounded-md bg-gray-100 border-gray-500 pl-10 text-base leading-5 transition focus:border-sky-300 focus:outline-none focus:ring-4 focus:ring-sky-300'
+            data-testid='custom-search'
+            placeholder='Search for roles or companies'
+            type='search'
+          ></input>
+        </div>
+        <div className='flex-1 mt-4 overflow-y-scroll'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </main>
     </div>
   )
 }
---
+++ src/components/ui/card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='h-[300px] bg-lime-200 flex items-center justify-center my-2'>
+      <div>Hii</div>
+    </div>
+  )
+}
+
+export default Card
